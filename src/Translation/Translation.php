dadc1ecc8046272ca3d2648d6465b40b4e1fc4b7
qbhy <572490755@qq.com>
2019-04-12 17:54:51 +0800

    添加百度和有道的翻译API

--- src/Translation/Translation.php
+++ src/Translation/Translation.php
@@ -7,22 +7,60 @@ use Qbhy\FreeApis\Api;
 
 class Translation extends Api
 {
-    public function google($text, $to = 'zh-CN')
+    public function google($text, $to)
     {
         return json_decode($this->app
             ->getClient()
             ->request('GET', "http://translate.google.cn/translate_a/single?client=gtx&dt=t&dj=1&ie=UTF-8&sl=auto&tl={$to}&q={$text}", [
                 RequestOptions::HEADERS => [
-                    'Referer'          => 'https://www.kuaidi100.com/',
+                    'Referer'          => 'https://translate.google.cn/',
                     'User-Agent'       => Api::AGENTS[random_int(0, count(Api::AGENTS) - 1)],
                     'Accept-Language'  => 'zh,zh-CN;q=0.9,en;q=0.8',
                     'Accept-Encoding'  => 'gzip, deflate, br',
                     'Connection'       => 'keep-alive',
                     'X-Requested-With' => 'XMLHttpRequest',
-                    'Host'             => 'www.kuaidi100.com',
-                    'Cookie'           => 'Hm_lvt_22ea01af58ba2be0fec7c11b25e88e6c=1554091249,1554099878; WWWID=WWW095E883265EF9D8F762C82F669FCF6EB; Hm_lpvt_22ea01af58ba2be0fec7c11b25e88e6c=1554107822'
+                    'Host'             => 'translate.google.cn',
                 ],
             ])
             ->getBody()->__toString(), true);
     }
+
+
+    public function baidu($text, $to)
+    {
+        return json_decode($this->app
+            ->getClient()
+            ->request('GET', "http://fanyi.baidu.com/transapi?from=auto&to={$to}&query={$text}", [
+                RequestOptions::HEADERS => [
+                    'Referer'          => 'https://translate.google.cn/',
+                    'User-Agent'       => Api::AGENTS[random_int(0, count(Api::AGENTS) - 1)],
+                    'Accept-Language'  => 'zh,zh-CN;q=0.9,en;q=0.8',
+                    'Accept-Encoding'  => 'gzip, deflate, br',
+                    'Connection'       => 'keep-alive',
+                    'X-Requested-With' => 'XMLHttpRequest',
+                    'Host'             => 'fanyi.baidu.com',
+                ],
+            ])
+            ->getBody()->__toString(), true);
+    }
+
+    public function youdao($text, $type = 'AUTO')
+    {
+        return json_decode($this->app
+            ->getClient()
+            ->request('GET', "http://fanyi.youdao.com/translate?&doctype=json&type={$type}&i={$text}", [
+                RequestOptions::HEADERS => [
+                    'Referer'          => 'http://fanyi.youdao.com/',
+                    'User-Agent'       => Api::AGENTS[random_int(0, count(Api::AGENTS) - 1)],
+                    'Accept-Language'  => 'zh,zh-CN;q=0.9,en;q=0.8',
+                    'Accept-Encoding'  => 'gzip, deflate, br',
+                    'Connection'       => 'keep-alive',
+                    'X-Requested-With' => 'XMLHttpRequest',
+                    'Host'             => 'fanyi.youdao.com',
+                ],
+            ])
+            ->getBody()->__toString(), true);
+    }
+
+
 }
