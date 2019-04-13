f135f7da01529b6726d1722e064e898187b6f5a4
qbhy <572490755@qq.com>
2019-04-13 18:32:34 +0800

    完善天气查询API

--- src/Weather/Weather.php
+++ src/Weather/Weather.php
@@ -13,9 +13,40 @@ class Weather extends Api
 {
     private $cityMap;
 
+    /**
+     * so json接口
+     *
+     * @param $cityId
+     *
+     * @return array
+     */
     public function query($cityId)
     {
         return json_decode($this->app->getClient()->get("http://t.weather.sojson.com/api/weather/city/{$cityId}")->getBody()->__toString(), true);
+    }
+
+    /**
+     * 中国天气网接口
+     *
+     * @param $cityId
+     *
+     * @return array
+     */
+    public function querySk($cityId)
+    {
+        return json_decode($this->app->getClient()->get("http://www.weather.com.cn/data/sk/{$cityId}.html")->getBody()->__toString(), true);
+    }
+
+    /**
+     * 中国天气网接口
+     *
+     * @param $cityId
+     *
+     * @return array
+     */
+    public function queryCityInfo($cityId)
+    {
+        return json_decode($this->app->getClient()->get("http://www.weather.com.cn/data/cityinfo/{$cityId}.html")->getBody()->__toString(), true);
     }
 
     /**
@@ -30,12 +61,31 @@ class Weather extends Api
         return $this->cityMap;
     }
 
-    public function queryWithCityName($cityName)
+    /**
+     * 通过城市名称获取城市ID
+     *
+     * @param $cityName
+     *
+     * @return string
+     * @throws WeatherException
+     */
+    public function getCityId($cityName)
     {
         if (isset($this->getCityMap()[$cityName])) {
-            return $this->query($this->getCityMap()[$cityName]);
+            return $this->getCityMap()[$cityName];
         }
 
-        throw new WeatherException('不支持的城市:' . $cityName,400);
+        throw new WeatherException('不支持的城市:' . $cityName, 400);
+    }
+
+    /**
+     * @param $cityName
+     *
+     * @return array
+     * @throws WeatherException
+     */
+    public function queryWithCityName($cityName)
+    {
+        return $this->query($this->getCityId($cityName));
     }
 }
